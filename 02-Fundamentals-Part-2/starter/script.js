e65b08f28f3f2a384efe0af0626898b52baeae4d
“Emilina <e.codreanu@students.clark.edu>
2023-02-07 11:15:18 -0500

    learned what strict is

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriverLicense = false;
+let passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log('I can drive');
+
+// let interface = 'audio';
+// let private = 534;
+
