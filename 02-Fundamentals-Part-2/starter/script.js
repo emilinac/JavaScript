d7072b3890dcd66737bb4aaac0056a14d1bc9062
“Emilina <e.codreanu@students.clark.edu>
2023-02-07 11:48:27 -0500

    learned about functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,29 @@
 'use strict';
-
-let hasDriverLicense = false;
-let passTest = true;
-
-if (passTest) hasDriverLicense = true;
-if (hasDriverLicense) console.log('I can drive');
+//
+// let hasDriverLicense = false;
+// let passTest = true;
+//
+// if (passTest) hasDriverLicense = true;
+// if (hasDriverLicense) console.log('I can drive');
 
 // let interface = 'audio';
 // let private = 534;
 
+// function logger (){
+//     console.log('My name is Emily!')
+// }
+// calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor (apples, oranges){
+    let juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+let appleJuice = fruitProcessor(5,0);
+console.log(appleJuice)
+
+let appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice)
